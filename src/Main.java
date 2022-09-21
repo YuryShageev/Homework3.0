8fb726efc601edf6d0cd50492d7d2dd77495e1f8
YuryShageev <schageev.yur@yandex.ru>
2022-09-21 17:51:25 +0300

    2-я домашка про переменные

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Целочисленные переменные:");
+        byte biteYourLeg = 5;
+        short shortPapa = 350;
+        int internalAffairs = 45000;
+        long longMama = 960000;
+        System.out.println("Значение переменной biteYourLeg с типом byte равно: "+ biteYourLeg);
+        System.out.println("Значение переменной shortPapa с типом short равно: " + shortPapa);
+        System.out.println("Значение переменной internalAffairs с типом int равно: " + internalAffairs);
+        System.out.println("Значение переменной longMama с типом long равно: " + longMama);
+        System.out.println("Переменные с плавающей точкой:");
+        float floatingRaft = 5.01f;
+        double doubleDecker = 8.0000001;
+        System.out.println("Значение переменной floatingRaft с типом float равно: " + floatingRaft);
+        System.out.println("Значение переменной doubleDecker с типом double равно: " + doubleDecker);
     }
 }
