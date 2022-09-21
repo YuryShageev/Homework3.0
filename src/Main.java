c8e8ed4422e8a06aaadbd6c00463eb3729788e3f
YuryShageev <schageev.yur@yandex.ru>
2022-09-21 17:54:20 +0300

    2-я домашка про переменные

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task 1!");
         System.out.println("Целочисленные переменные:");
         byte biteYourLeg = 5;
         short shortPapa = 350;
