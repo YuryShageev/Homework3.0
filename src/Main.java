f79540b334e28071484a2855618fb0e55077109c
YuryShageev <schageev.yur@yandex.ru>
2022-09-21 18:19:29 +0300

    2-я домашка про переменные

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         byte biteYourLeg = 5;
         short shortPapa = 350;
         int internalAffairs = 45000;
-        long longMama = 960000;
+        long longMama = 960000L;
         System.out.println("Значение переменной biteYourLeg с типом byte равно: "+ biteYourLeg);
         System.out.println("Значение переменной shortPapa с типом short равно: " + shortPapa);
         System.out.println("Значение переменной internalAffairs с типом int равно: " + internalAffairs);
@@ -15,5 +15,24 @@ public class Main {
         double doubleDecker = 8.0000001;
         System.out.println("Значение переменной floatingRaft с типом float равно: " + floatingRaft);
         System.out.println("Значение переменной doubleDecker с типом double равно: " + doubleDecker);
+
+        System.out.println("Task 2!");
+        byte b1 = 67;
+        short s1 = 549;
+        short s2 = -159;
+        int i1 = 27897;
+        long l1 = 987678965549L;
+        float f1 = 27.12f;
+        double d1 = 2.786;
+        boolean boo1 = b1 >= s1;
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(i1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(boo1);
+
     }
 }
